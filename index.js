4038c66429d583ffd4c504f39d91c154739dc962
Max Ogden <max@maxogden.com>
2014-01-08 12:24:14 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var http = require('http')
 var hat = require('hat')
-http.createServer(function(req, res) { res.end(hat()) }).listen(8081)
+http.createServer(function(req, res) { res.end('num: ' + hat()) }).listen(8081)
